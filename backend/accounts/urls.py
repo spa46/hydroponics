768015e7b4e400b9c5c459abc9f87a4ef78f7800
seansc.park <seansc.park@lge.com>
2022-08-24 20:13:31 +0900

    [Backend] Accounts: Authentications

---
+++ backend/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+# from . import views
+
+urlpatterns = [
+    path('', include('dj_rest_auth.urls')),
+    path('registration', include('dj_rest_auth.registration.urls')),
+]
+
